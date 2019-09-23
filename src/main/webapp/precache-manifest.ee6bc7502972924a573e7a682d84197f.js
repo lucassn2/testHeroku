18e23c57409fad5cb4bc9bc205a8066c91d787e2
lucassn2 <lugispil@spangholmnielsen.dk>
2019-09-23 17:13:01 +0200

    Updated webapp

---
+++ src/main/webapp/precache-manifest.ee6bc7502972924a573e7a682d84197f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27debad069edc2c8aaf56fa32a915de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b1ed225e1dea397d78d",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2e1d9cc27791ac614d15",
+    "url": "/static/js/2.8ce7f97d.chunk.js"
+  },
+  {
+    "revision": "8b1ed225e1dea397d78d",
+    "url": "/static/js/main.8f221a59.chunk.js"
+  },
+  {
+    "revision": "3eb89e8c880c493c93ea",
+    "url": "/static/js/runtime-main.0b761a9c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
